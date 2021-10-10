0b774eba3cd7dc6710455cc8c591f5c67fcfc9b1
Bettina Benitez <bettinabenitez16@gmail.com>
2021-10-09 21:13:32 -0700

    fixed the add button to incorporate autofocus

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import List from './List';
 import './styles.css';
 
 function App() {
-  const data = [
+  const initialData = [
     {
       id: 1,
       todo: "Call mom",
       completed: true,
+      focus: false,
     },
     {
       id: 2,
       todo: "Eat lunch",
       completed: false,
+      focus: true,
     }
   ]
+
+  const [data, setData] = useState(initialData);
+  const [adding, setAdding] = useState(false);
+  const [editing, setEditing] = useState(false);
+
+  function plusClicked() {
+      let newId = data.length;
+      let newData = data;
+
+      newData.push({
+          id: newId,
+          todo: "",
+          completed: false,
+          focus: true,
+      })
+
+      setData(newData);
+      setAdding(true);
+  }
+
+  function doneClicked() {
+    let newData = data;
+    {data[data.length-1].todo === "" && newData.pop()}
+    setData(newData);
+    {adding == false ? setEditing(false) : setAdding(true)};
+  }
+
+  // function editClicked(){
+  //   setEditing(true);
+  // }
 
   return (
     <>
@@ -23,11 +55,27 @@ function App() {
               <h1 className="underline">To-do list</h1>
           </div>
           <div className="buttons">
-              <h1>Edit</h1>
-              <h1>+</h1>
+              {adding == true || editing == true ? 
+                  (<button onClick={doneClicked}>Done</button>)
+                  : 
+                  (
+                    <>
+                    <button onClick={setEditing(true)}>Edit</button>
+                    <button onClick={plusClicked}>+</button> 
+                    </>
+                  )
+              }
           </div>
       </div>
-      <List data={data} filterType="showCompleted"/>
+      <List
+        data={data} 
+        filterType="showCompleted" 
+        setData={(index, isComplete) => {
+          let newData = data;
+          newData[index].completed = isComplete;
+          setData(newData);
+        }}
+      />
     </>
   );
 }
--- lab2/src/List.js
+++ lab2/src/List.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from 'react';
 
 function List(props) {
+    const searchInput = useRef(null);
+
+    useEffect(()=>{
+        searchInput.current.focus();
+     },[])
+
     const filteredData = props.filterType === 'showCompleted' ? props.data : props.data.filter(item => !item.completed)
+
+    function handleChange(e, index) {
+        let isChecked = e.target.checked;
+        props.setData(isChecked, index);
+    }
+
     return(
         <div>
             {filteredData.map((item, i) => (
                 <div className = "input" id={i}>
-                    <input type="checkbox"/>
-                    <label><h1>{item.todo}</h1></label>
+                <input type="checkbox" onChange={handleChange(e, index)}/>
+                    {item.focus == true ? 
+                        <input ref={searchInput} value={item.todo}/> :
+                        <input value={item.todo} readOnly={true}/>
+                    }
                 </div>
             ))}
         </div>
