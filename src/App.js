07d59d088ae3c982925479d8ce5a1ef7a153ee08
natashawong <natasha.wong10@gmail.com>
2021-10-20 14:23:32 -0700

    setting up db

--- src/App.js
+++ src/App.js
@@ -1,41 +1,70 @@
 import React, { useState } from 'react';
 import List from './List';
+import firebase from "firebase/compat";
+import { useCollection } from "react-firebase-hooks/firestore";
+import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
+import { map } from '@firebase/util';
 
 import './styles.css';
 import {filterType, modeType} from './Constants';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyATi5uqr6dkf4iTvqfPHndGwtHXz0O6O-s",
+  authDomain: "lab-90286.firebaseapp.com",
+  databaseURL: "https://lab-90286-default-rtdb.firebaseio.com",
+  projectId: "lab-90286",
+  storageBucket: "lab-90286.appspot.com",
+  messagingSenderId: "782831139249",
+  appId: "1:782831139249:web:2d846e81abb155eeece590",
+  measurementId: "G-YTZ7W2BRD9"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
 function App() {
-  const initialData = [
-    {
-      id: 1,
-      todo: "Call mom",
-      completed: false,
-    },
-    {
-      id: 2,
-      todo: "Eat lunch",
-      completed: false,
-    }
-  ]
+  const collection = db.collection('natasha-bettina-124');
+  console.log(collection);
+  const [value, loading, error] = useCollection(collection);
+  const initialData = value ? value : [{}];
 
   const [data, setData] = useState(initialData);
   const [filter, setFilterType] = useState(filterType.showAll);
   const [mode, setMode] = useState(modeType.base);
   const [atLeastOneSelected, setAtLeastOneSelected ] = useState(false);
-  const [latestId, setLatestId] = useState(3);
+
+  function addTodo() {
+    // Adds an empty Todo
+    const newId = generateUniqueID();
+    console.log(newId);
+    console.log("coll", collection.doc(newId));
+    collection.doc(newId).set({ id: newId, todo: "", completed: false});
+    console.log(value);
+  }
+
+  function removeTodo(id) {
+    collection.doc(id).delete();
+  }
+
+  function editTodo(todoObj) {
+    // Edits an existing Todo
+    // todoObj is {id:..., todo:..., completed:..}
+    collection.doc(todoObj.id).update(todoObj);
+  }
 
   function plusClicked() {
-      let newData = data;
+      // let newData = data;
 
-      newData.push({
-          id: latestId,
-          todo: "",
-          completed: false,
-      })
-      let newId = latestId + 1;
-      setLatestId(newId);
+      // newData.push({
+      //     id: latestId,
+      //     todo: "",
+      //     completed: false,
+      // })
+      // let newId = latestId + 1;
+      // setLatestId(newId);
 
-      setData(newData);
+      // setData(newData);
+      addTodo();
       setMode(modeType.add);
   }
 
